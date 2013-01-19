7dc6c487a3041b6a6b19e6551f9aa69d2253a795
Chad Schulz <chad.awesome@gmail.com>
2013-01-19 16:46:34 -0600

    + test harness samples, key panel

--- js/diagnostics.js
+++ js/diagnostics.js
@@ -10,16 +10,22 @@ var diag = function() {
     'div:empty, span:empty,',
     'li:empty, p:empty,',
     'td:empty, th:empty {padding: 0.5em; background: yellow;}',
+
     '*[style], font, center {outline: 5px solid red;}',
     '*[class=""], *[id=""] {outline: 5px dotted red;}',
+
     'img[alt=""] {border: 3px dotted red;}',
     'img:not([alt]) {border: 5px solid red;}',
+
     'img[title=""] {outline: 3px dotted fuchsia;}',
     'img:not([title]) {outline: 5px solid fuchsia;}',
-    'table:not([summary]) {outline: 5px solid red;}',
+
     'table[summary=""] {outline: 3px dotted red;}',
+    'table:not([summary]) {outline: 5px solid red;}',
+
     'th {border: 2px solid red;}',
     'th[scope="col"], th[scope="row"] {border: none;}',
+
     'a[href]:not([title]) {border: 5px solid red;}',
     'a[title=""] {outline: 3px dotted red;}',
     'a[href="#"] {background: lime;}',
@@ -27,7 +33,22 @@ var diag = function() {
   ];
 
   // Key listing
-  this.k = []
+  this.k = [
+    ['Empty element: ', 'yellow background', 'yellow'],
+    ['Inline styling: ', 'solid red border', 'red'],
+    ['Empty class or id: ', 'dotted red border', 'red'],
+    ['&lt;img&gt; empty alt: ', 'dotted red border', 'red'],
+    ['&lt;img&gt; missing alt: ', 'solid red border', 'red'],
+    ['&lt;img&gt; empty title: ', 'dotted fuchsia border', 'fuchsia'],
+    ['&lt;img&gt; missing title: ', 'solid fuchsia border', 'fuchsia'],
+    ['&lt;table&gt; empty summary: ', 'dotted red border', 'red'],
+    ['&lt;table&gt; missing summary: ', 'solid red border', 'red'],
+    ['&lt;th&gt; mis-set scope: ', 'solid red border', 'red'],
+    ['&lt;a&gt; missing title: ', 'solid red border', 'red'],
+    ['&lt;a&gt; empty title: ', 'dotted red border', 'red'],
+    ['&lt;a&gt; href set to #: ', 'lime background', 'lime'],
+    ['&lt;a&gt; empty href: ', 'fuchsia background', 'fuchsia']
+  ];
 
 };
 
@@ -49,13 +70,13 @@ diag.prototype.run = function() {
 diag.prototype.rules = function() {
   var p = document.createElement('div');
   p.className = 'diagnostics-panel-rules';
-  var style =[];
+  var els =[];
 
   for (var i=0; i<this.r.length; i++) {
-   style.push('<p>' + this.r[i] + '</p>');
+   els.push('<p>' + this.r[i] + '</p>');
   }
 
-  p.innerHTML = style.join('');
+  p.innerHTML = els.join('');
 
   document.body.appendChild(p);
 
@@ -68,7 +89,29 @@ diag.prototype.rules = function() {
  * key() shows the key
  */
 diag.prototype.key = function() {
+  var p = document.createElement('div');
+  var s = document.createElement('style');
+  p.className = 'diagnostics-panel-key';
 
+  var key = [],
+      sty = [];
+
+  // Add class-based styling to the document
+  for (var i=0; i<this.k.length; i++) {
+   sty.push('.' + this.k[i][2] + '{color:'
+    + this.k[i][2] + ';}');
+  }
+  s.innerHTML = sty.join('');
+
+  for (var i=0; i<this.k.length; i++) {
+   key.push('<p>' + this.k[i][0] + '<span class="'
+    + this.k[i][2] + '">' + this.k[i][1] + '</span></p>');
+  }
+
+  p.innerHTML = key.join('');
+
+  document.head.appendChild(s);
+  document.body.appendChild(p);
 
   // Logging
   console.log('Show key...');
@@ -80,5 +123,6 @@ window.diagnostics = window.diagnostics || new diag;
 
 (function() {
   diagnostics.run();
-  diagnostics.rules();
+  //diagnostics.rules();
+  diagnostics.key();
 })();
